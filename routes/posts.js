3f83efa4529367800b08041378ec5401467936c2
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-05 22:01:16 +0200

    Add posts/postId route

--- routes/posts.js
+++ routes/posts.js
@@ -6,4 +6,8 @@ router.get("/", (req, res, next) => {
   res.send("NOT IMPLEMENTED: /posts GET");
 });
 
+router.get("/:postId", (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: /posts/${req.params.postId} GET`);
+});
+
 module.exports = router;
