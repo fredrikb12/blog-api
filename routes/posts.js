6dc35fc35e53302a45b1e39da6b36dd054b77830
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-05 22:09:41 +0200

    Add remaining posts routes

--- routes/posts.js
+++ routes/posts.js
@@ -2,12 +2,24 @@ const express = require("express");
 
 const router = express.Router();
 
+router.post("/", (req, res, next) => {
+  res.send("NOT IMPLEMENTED: /posts POST");
+});
+
 router.get("/", (req, res, next) => {
   res.send("NOT IMPLEMENTED: /posts GET");
 });
 
 router.get("/:postId", (req, res, next) => {
   res.send(`NOT IMPLEMENTED: /posts/${req.params.postId} GET`);
+});
+
+router.put("/:postId", (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: /posts/${req.params.postId} PUT`);
+});
+
+router.delete("/:postId", (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: /posts/${req.params.postId} DELETE`);
 });
 
 module.exports = router;
