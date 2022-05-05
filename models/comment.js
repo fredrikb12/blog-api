a55b42dff1806dccf4593799be17660b885210c8
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-05 19:53:22 +0200

    Create comment model

---
+++ models/comment.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema(
+  {
+    author: { type: String, required: true },
+    text: { type: String, required: true },
+    createdAt: Number,
+    updatedAt: Number,
+  },
+  {
+    timestamps: { currentTime: () => Date.now() },
+  }
+);
