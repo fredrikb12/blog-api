84a614dd739e85613eb289a4f08582383c3eb335
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-06 11:22:14 +0200

    Edit max length properties

--- models/post.js
+++ models/post.js
@@ -3,13 +3,12 @@ const Schema = mongoose.Schema;
 
 const PostSchema = new Schema(
   {
-    title: { type: String, required: true },
-    text: { type: String, required: true },
+    title: { type: String, required: true, maxLength: 100 },
+    text: { type: String, required: true, maxLength: 2000 },
     author: {
       type: Schema.Types.ObjectId,
       required: true,
       ref: "User",
-      maxLength: 2000,
     },
     published: { type: Boolean, required: true },
     createdAt: Number,
