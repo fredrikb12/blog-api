6fe917ba138a8467e34bf72b61cda472727f6fbe
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-06 14:47:54 +0200

    Implement signup POST route

--- routes/auth.js
+++ routes/auth.js
@@ -6,4 +6,8 @@ router.post("/sign-up", (req, res, next) => {
   res.send("NOT IMPLEMENTED: /sign-up POST");
 });
 
+router.post("/sign-in", (req, res, next) => {
+  res.send("NOT IMPLEMENTED: /sign-in POST");
+});
+
 module.exports = router;
