f20208d225ccb150779757a0b79fbb163d890311
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-06 19:07:53 +0200

    Refactor to use controller methods

--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.post("/signup", authController.signup_POST);
 
-router.post("/signin", (req, res, next) => {
-  res.send("NOT IMPLEMENTED: /sign-in POST");
-});
+router.post("/signin", authController.signin_POST);
+
+router.post("/posts", authController.posts_POST);
 
 module.exports = router;
